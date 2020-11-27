5bf7d968c9f6dfcc568ca774341538c2f48a88ae
DOUVILLE pierre <pierre.douville2707@gmail.com>
2020-11-27 22:54:04 +0100

    burgerCode

---
+++ admin/delete.php
@@ -0,0 +1,58 @@
+<?php
+  require 'database.php';
+  if(!empty($_GET['id'])){
+    $id = checkInput($_GET['id']);
+  }
+
+  if(!empty($_POST)) {
+    $id = checkInput($_POST['id']);
+    $db = Database::connect();
+    $statement = $db->prepare("DELETE FROM items WHERE id = ?");
+    $statement->execute(array($id));
+    Database::disconnect();
+    header("Location: index.php");
+  }
+
+  function checkInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <title>Burger Code</title>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+     <link rel="stylesheet" href="../css/styles.css?t=<? echo time(); ?>" media="all">
+     <link href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&display=swap" rel="stylesheet">
+   </head>
+
+   <body>
+     <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
+     <div class="container admin">
+       <div class="row">
+         <h1><strong>Supprimer un item</strong></h1>
+         <br>
+         <form class="form" role="form" action="delete.php" method="post">
+           <input type="hidden" name="id" value="<?php echo $id ?>">
+           <p class="alert alert-warning">Etes vous sur de vouloir supprimer</p>
+
+              <div class="form-actions">
+                 <button type="submit" class="btn btn-warning">oui</button>
+                 <a class="btn btn-default" href="index.php">non</a>
+             </div>
+          </form>
+
+        </div>
+      </div>
+
+   </body>
+
+ </html>
